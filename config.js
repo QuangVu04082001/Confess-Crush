3205a856556cf4d4f71d2a12f02d49bd324c2807
QuangVu04082001 <92837815+QuangVu04082001@users.noreply.github.com>
2021-10-20 10:12:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Nhân Ngày 20/11 của bé Kim",
+    introTitle: 'Bé Kim Ngang Ngược',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -8,13 +8,13 @@ const CONFIG = {
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    btnYes: 'Thích quá trời lun <33',
+    btnNo: 'Không nha anh chai :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu bé Quang lo chyện bao đồng vậy chèn <3',
+    btnReply: 'Gửi cho bé KIm của anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Nay 20/11 là ngày đẹp, lần trước là em cua anh thì lần này anh cua lại thế giới của mình. Làm Bà Xã của anh một lần nữa nha bé KIM ngang ngược mà vân quan tâm anh chai hơm hỉu chuyện này.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
